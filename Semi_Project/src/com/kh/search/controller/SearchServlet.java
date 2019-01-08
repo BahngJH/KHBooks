4a2e5f973e02338b407099256942272b8b390ce5
sinna94 <sinna94@naver.com>
2019-01-08 15:14:30 +0900

    검색 기능 디비 연동 중

---
+++ Semi_Project/src/com/kh/search/controller/SearchServlet.java
@@ -0,0 +1,48 @@
+package com.kh.search.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.book.model.vo.Book;
+import com.kh.search.model.service.SearchService;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/search/search")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String key = request.getParameter("key");
+		
+		List<Book> list = new SearchService().selectBook(key);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
