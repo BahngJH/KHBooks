7f642e88c37134a4d556c0771f284e95c89e8391
주홍범 <@DESKTOP-6J5UPT1>
2019-01-13 22:48:44 +0900

    리뷰 관리 페이지 구현 , 리뷰 서블릿 추가 리뷰 Dao, Service 클래스 추가

---
+++ Semi_Project/src/com/kh/review/model/service/ReviewService.java
@@ -0,0 +1,21 @@
+package com.kh.review.model.service;
+
+import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+import java.util.List;
+
+import com.kh.review.model.dao.ReviewDao;
+import com.kh.review.model.vo.Review;
+
+public class ReviewService {
+	
+	public List<Review> selectList(int memberNum) {
+		Connection conn = getConnection();
+		List<Review> list = new ReviewDao().selectList(conn, memberNum);
+		
+		close(conn);
+		return list;
+	}
+}
