8db8668cfdfdf763f6058cca2af75b3901bde979
user1 <user1@user1-PC>
2019-01-15 15:17:32 +0900

    notice 공지사항 삭제 수정 구현 삭제기능은 완료하였으나 페이지 경로 설정 다시

---
+++ Semi_Project/src/com/kh/notice/controller/NoticeContentServlet.java
@@ -0,0 +1,53 @@
+package com.kh.notice.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.notice.model.service.NoticeService;
+import com.kh.notice.model.vo.Notice;
+
+/**
+ * Servlet implementation class NoticeContentServlet
+ */
+@WebServlet("/notice/noticeContent")
+public class NoticeContentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoticeContentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int no = Integer.parseInt(request.getParameter("no"));
+		
+		Notice n = new NoticeService().selectNo(no);
+		
+		request.setAttribute("n", n);
+		
+		request.getRequestDispatcher("/views/notice/noticeContent.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Semi_Project/src/com/kh/notice/controller/NoticeDeleteServlet.java
@@ -0,0 +1,70 @@
+package com.kh.notice.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.notice.model.service.NoticeService;
+import com.kh.notice.model.vo.Notice;
+
+/**
+ * Servlet implementation class NoticeDeleteServlet
+ */
+@WebServlet("/notice/noticedelete")
+public class NoticeDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoticeDeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int no=Integer.parseInt(request.getParameter("no"));	
+		
+		int result = new NoticeService().deleteNotice(no);
+			System.out.println(no);
+		String msg="";
+		String view ="/views/common/msg.jsp";
+		String loc="";
+		if(result>0) {
+			msg="공지사항 삭제완료!";
+			loc="/views/notrice/mainnotice";
+				
+				
+			}else{
+				request.setAttribute("msg","삭제 실패");
+			}
+		request.setAttribute("msg", msg);
+		request.setAttribute("loc", loc);
+		
+		request.getRequestDispatcher(view).forward(request, response);
+			
+			
+			
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
