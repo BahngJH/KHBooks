bb47c26e9a1f24978d018a78cd784d9a748ad311
user1 <user1@user1-PC>
2019-01-18 14:55:55 +0900

    황장운

--- Semi_Project/src/com/kh/info/model/service/InfoService.java
+++ Semi_Project/src/com/kh/info/model/service/InfoService.java
@@ -6,7 +6,6 @@ import static common.JDBCTemplate.getConnection;
 import java.sql.Connection;
 import java.util.List;
 
-import com.kh.author.model.vo.Author;
 import com.kh.book.model.vo.Book;
 import com.kh.info.model.dao.InfoDao;
 import com.kh.review.model.vo.Review;
@@ -27,5 +26,22 @@ public class InfoService {
 		List<Review> list=new InfoDao().selectInfoReview(conn, bookId);
 		close(conn);
 		return list;
+	}
+	
+	public int selectReviewCount()
+	{
+		Connection conn=getConnection();
+		int result=new InfoDao().selectReviewCount(conn);
+		close(conn);
+		return result;
+	}
+	
+	public List<Review> selectReviewList(int cPage, int numPerPage)
+	{
+		Connection conn=getConnection();
+		List<Review> reviewCountList=new InfoDao().selectReviewList(conn, cPage, numPerPage);
+		close(conn);
+		System.out.println(reviewCountList.size());
+		return reviewCountList;
 	}
 }
