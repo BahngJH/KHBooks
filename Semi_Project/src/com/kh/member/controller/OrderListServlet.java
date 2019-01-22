f27d3af9becc7e8691dd9011b0b5e7e071a9052c
주홍범 <@DESKTOP-6J5UPT1>
2019-01-22 21:41:11 +0900

    구매목록 페이징 처리

--- Semi_Project/src/com/kh/member/controller/OrderListServlet.java
+++ Semi_Project/src/com/kh/member/controller/OrderListServlet.java
@@ -84,7 +84,7 @@ public class OrderListServlet extends HttpServlet {
 		{
 			if(cPage==pageNo)
 			{
-				pageBar+="<li><span class='cPage'>"+pageNo+"</span></li>";
+				pageBar+="<li class='page-item active'><span class='cPage'>"+pageNo+"</span></li>";
 			}
 			else
 			{
